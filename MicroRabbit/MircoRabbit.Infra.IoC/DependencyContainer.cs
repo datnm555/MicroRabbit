337e0cd55b3c7a1d6e96d5b9ced80902829defbe
mydatng <mydatng@gmail.com>
2022-07-01 01:19:04 +0700

    implementation ifra ioc project

---
+++ MicroRabbit/MircoRabbit.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,17 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MircoRabbit.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterSevices(IServiceCollection services)
+        {
+            services.AddTransient<IEventBus, RabbitMQBus>();
+        }
+    }
+}
