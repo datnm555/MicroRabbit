6239d05822d584f35eb421dff39e2a2b3c95b410
mydatng <mydatng@gmail.com>
2022-07-01 00:42:49 +0700

    Implement intercface and class for  domain.core

---
+++ MicroRabbit/MicroRabbit.Domain.Core/Commands/Command.cs
@@ -0,0 +1,18 @@
+﻿using MicroRabbit.Domain.Core.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core.Commands
+{
+    public class Command : Message
+    {
+        public Command()
+        {
+            Timestamp = DateTime.Now;
+        }
+
+        public DateTime Timestamp { get; protected set; }
+
+    }
+}
